894c5391d288a337393448a3da90f6ebbbd04001
IzaiasCF <izaiascorreafilho@gmail.com>
2025-09-18 10:46:56 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppQuiz/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppQuiz
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiAppQuiz/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppQuiz
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
